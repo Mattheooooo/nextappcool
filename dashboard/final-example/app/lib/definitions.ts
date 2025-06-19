e86e888303303d1b112c29f794842b7040ecbaff
mcoppin <mattheo.coppin@rossel.fr>
2025-06-19 11:02:06 +0200

    fix: mon push a pas marcher reseau de ptnnnnnnnnnnnn

--- dashboard/final-example/app/lib/definitions.ts
+++ dashboard/final-example/app/lib/definitions.ts
@@ -38,7 +38,7 @@ export type LatestInvoice = {
   email: string;
   amount: string;
   date: string;
-}; //commentaire useless 2 aaaaaaaaa
+}; //commentaire useless 2 jpp push purée
 
 export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
   amount: number;
